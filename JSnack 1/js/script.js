f34fcd7785def153ca4cc48ffe4de8029c92eba0
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-30 13:01:06 +0100

    init project

---
+++ JSnack 1/js/script.js
@@ -0,0 +1,10 @@
+// console.log("Ciao Kikka")
+
+// Snack1
+// Ottieni un numero casuale in un range specificato. Chiedi all'utente il valore minimo ed il valore massimo del range in cui generare il numero.
+
+const random_number = Math.floor(Math.random() * 10) + 1;
+console.log(random_number);
+
+let min_number = parseInt(prompt("inserisci valore minimo"));
+let max_number = parseInt(prompt("inserisci valore massimo"));
---
+++ JSnack 4/js/script.js
@@ -0,0 +1,4 @@
+// console.log("Ciao Kikka")
+
+// Snack4 (Bonus)
+// Fai inserire un numero all'utente. Poi continua a chiedere numeri finché la serie è crescente (l'ultimo numero inserito deve essere maggiore del precedente). Alla fine stampali tutti.
